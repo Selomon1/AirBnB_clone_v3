711a46af91e6460111998f21a7c858a902a16db5
Selomon1 <soya.vandy1950@gmail.com>
2024-01-09 22:40:04 +0300

    RESTApi

---
+++ api/v1/views/states.py
@@ -0,0 +1,62 @@
+#!usr/bin/python3
+""" State object viewws """
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage, State
+
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_states():
+    """ Retrieve all states """
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """ Retrieve a specific state """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state-id):
+    """ delete a state """
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    storage.delete(state)
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/states', method=['POST'], strict_slashes=False)
+def create_state():
+    """ Create a new state """
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    if 'name' not in data:
+        abort(400, "Missing name")
+    n_state = State(**data)
+    n_state.save()
+    return jsonify(n_state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """ Update a state """
+    data = request.get_json()
+    if data is None:
+        abort(400, "Not a JSON")
+    state = storage.get(State, state_id)
+    if state is None:
+        abort(404)
+    for key, value in data.items():
+        if key not in ['id', 'created_at', 'updated_at']:
+            setattr(state, key, value)
+    state.save()
+    return jsonify(state.to_dict())
